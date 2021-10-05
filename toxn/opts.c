cd94417b4cb38f0fdf74c7989b26b4dd7a523f41
necro-nemesis <necro_nemesis@hotmail.com>
2021-10-04 21:04:07 -0400

    set default port to 6680

--- toxn/opts.c
+++ toxn/opts.c
@@ -6,7 +6,7 @@ int net_max_uploads = -2;
 unsigned net_max_peers;
 unsigned net_bw_limit_in;
 unsigned net_bw_limit_out;
-int net_port = 6881;
+int net_port = 6880;
 off_t cm_alloc_size = 2048 * 1024;
 int ipcprot = 0600;
 int empty_start = 0;
